ce75cd65d6e39c20ea2cf29aeb7bb847e57c1179
Ashirwad Sangwan <ashirwadsangwan@gmail.com>
2019-06-04 14:46:43 +0530

    lesson 2 done!!!

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Ashirwad Sangwan',
+        'author': 'Ashirwad',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Girika Ganguly',
+        'author': 'Ashirwad',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
@@ -18,16 +17,15 @@ posts = [
 ]
 
 
-@app.route('/')
-@app.route('/home')
-
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html', posts=posts)
 
-@app.route('/about')
 
+@app.route("/about")
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
